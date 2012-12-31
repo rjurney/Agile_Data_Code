2e9f50a4a0b61a5d7f5b1dd4749aed0d5e9616f6
Russell Jurney <russell.jurney@gmail.com>
2012-12-31 12:20:17 -0800

    Added bootstrap enabled total sent_counts example. Better, newer CSS.

---
+++ ch3/web/index.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import pymongo
+import json
+import re
+
+# Setup Flask
+app = Flask(__name__)
+
+# Setup Mongo
+conn = pymongo.Connection() # defaults to localhost
+db = conn.agile_data
+
+# Fetch from/to totals and list them
+@app.route("/sent_counts")
+def sent_counts():
+  sent_counts = db['sent_counts'].find()
+  results = {}
+  results['keys'] = 'from', 'to', 'total'
+  results['values'] = [[s['from'], s['to'], s['total']] for s in sent_counts if re.search('apache', s['from']) or re.search('apache', s['to'])]
+  results['values'] = results['values'][0:17]
+  return render_template('table.html', results=results)
+
+if __name__ == "__main__":
+  app.run(debug=True)
