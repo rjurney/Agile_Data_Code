2db69b7eef5a547262c4a6ff143d4d4bf3a7747b
Russell Jurney <russell.jurney@gmail.com>
2013-01-09 15:25:08 -0800

    Extended mongo code

--- ch7/list_emails.mongo.js
+++ ch7/list_emails.mongo.js
@@ -40,3 +40,32 @@ db.emails.getIndexes();
 // ]
 db.emails.find().sort({date: 1});
 // ... lots of sorted emails ...
+db.emails.ensureIndex({message_id: 1}); // Add message_id index
+db.emails.getIndexes();
+// [
+//  {
+//    "v" : 1,
+//    "key" : {
+//      "_id" : 1
+//    },
+//    "ns" : "agile_data.emails",
+//    "name" : "_id_"
+//  },
+//  {
+//    "v" : 1,
+//    "key" : {
+//      "date" : 1
+//    },
+//    "ns" : "agile_data.emails",
+//    "name" : "date_1"
+//  },
+//  {
+//    "v" : 1,
+//    "key" : {
+//      "message_id" : 1
+//    },
+//    "ns" : "agile_data.emails",
+//    "name" : "message_id_1"
+//  }
+// ]
+db.emails.find().sort({date:0}).limit(10).pretty(); // Fetch last 10 emails, pretty format
