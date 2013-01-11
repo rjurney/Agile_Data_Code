cc087e24a6b1bb1961a477975af4144d2c837738
Russell Jurney <russell.jurney@gmail.com>
2013-01-11 14:53:49 -0800

    Match query in elasticsearch, sort on date descending

--- ch7/elasticsearch.py
+++ ch7/elasticsearch.py
@@ -2,4 +2,7 @@ import pyelasticsearch
 elastic = pyelasticsearch.ElasticSearch('http://localhost:9200/inbox')
 results = elastic.search("hadoop", index="emails")
 print results
+results2 = elastic.search({'query': {"term": { "body": query}}, 'from': 0, 'size': 20}, index="emails")
+print results2
+
 
