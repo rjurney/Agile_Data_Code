f00a81089fd9140a3da61524c5982368c48b0544
Russell Jurney <russell.jurney@gmail.com>
2013-01-13 19:24:22 -0800

    Updated mongo script with index for sent_distributions

--- ch8/mongo.js
+++ ch8/mongo.js
@@ -39,6 +39,7 @@ db.addresses_per_email.findOne()
 //    }
 //  ]
 // }
+db.sent_distributions.ensureIndex({sender_email_address: 1})
 db.sent_distributions.findOne()
 // {
 //  "_id" : ObjectId("50f365ba30042ade8f22cb86"),
@@ -52,93 +53,6 @@ db.sent_distributions.findOne()
 //      "sent_hour" : "01",
 //      "total" : NumberLong(307)
 //    },
-//    {
-//      "sent_hour" : "02",
-//      "total" : NumberLong(196)
-//    },
-//    {
-//      "sent_hour" : "03",
-//      "total" : NumberLong(147)
-//    },
-//    {
-//      "sent_hour" : "04",
-//      "total" : NumberLong(144)
-//    },
-//    {
-//      "sent_hour" : "05",
-//      "total" : NumberLong(120)
-//    },
-//    {
-//      "sent_hour" : "06",
-//      "total" : NumberLong(102)
-//    },
-//    {
-//      "sent_hour" : "07",
-//      "total" : NumberLong(142)
-//    },
-//    {
-//      "sent_hour" : "08",
-//      "total" : NumberLong(188)
-//    },
-//    {
-//      "sent_hour" : "09",
-//      "total" : NumberLong(293)
-//    },
-//    {
-//      "sent_hour" : "10",
-//      "total" : NumberLong(410)
-//    },
-//    {
-//      "sent_hour" : "11",
-//      "total" : NumberLong(503)
-//    },
-//    {
-//      "sent_hour" : "12",
-//      "total" : NumberLong(657)
-//    },
-//    {
-//      "sent_hour" : "13",
-//      "total" : NumberLong(698)
-//    },
-//    {
-//      "sent_hour" : "14",
-//      "total" : NumberLong(755)
-//    },
-//    {
-//      "sent_hour" : "15",
-//      "total" : NumberLong(730)
-//    },
-//    {
-//      "sent_hour" : "16",
-//      "total" : NumberLong(713)
-//    },
-//    {
-//      "sent_hour" : "17",
-//      "total" : NumberLong(745)
-//    },
-//    {
-//      "sent_hour" : "18",
-//      "total" : NumberLong(655)
-//    },
-//    {
-//      "sent_hour" : "19",
-//      "total" : NumberLong(607)
-//    },
-//    {
-//      "sent_hour" : "20",
-//      "total" : NumberLong(555)
-//    },
-//    {
-//      "sent_hour" : "21",
-//      "total" : NumberLong(589)
-//    },
-//    {
-//      "sent_hour" : "22",
-//      "total" : NumberLong(514)
-//    },
-//    {
-//      "sent_hour" : "23",
-//      "total" : NumberLong(482)
-//    }
+//    ...
 //  ]
 // }
