08678c9595b5a94dada15a0f6af2fb80203190bb
Russell Jurney <russell.jurney@gmail.com>
2013-01-27 19:49:51 -0800

    Finished chapter 10 fixes with pig fix, now working on topic extraction.

---
+++ ch9/udfs.py
@@ -0,0 +1,14 @@
+@outputSchema("sent_dist:bag{t:(sent_hour:chararray, total:int)}")
+def fill_in_blanks(sent_dist):
+  print sent_dist
+  out_data = list()
+  hours = [ '%02d' % i for i in range(24) ]
+  for hour in hours:
+    entry = [x for x in sent_dist if x[0] == hour]
+    if entry:
+      entry = entry[0]
+      print entry.__class__
+      out_data.append(tuple([entry[0], entry[1]]))
+    else:
+      out_data.append(tuple([hour, 0]))
+  return out_data
