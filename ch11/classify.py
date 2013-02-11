f51a1bebc0f113d33f7f481fb399849cf63c6726
Russell Jurney <russell.jurney@gmail.com>
2013-02-10 18:26:08 -0800

    Working from/to & token classifier.

--- ch11/classify.py
+++ ch11/classify.py
@@ -6,33 +6,46 @@ from nltk.tokenize import word_tokenize
 conn = pymongo.Connection() # defaults to localhost
 db = conn.agile_data
 from_to_reply_ratios = db['from_to_reply_ratios']
-hourly_from_reply_probs = db['hourly_from_reply_probs']
-p_sent_from_to = db['p_sent_from_to']
+token_reply_rates = db['token_reply_rates']
 overall_reply_ratio = db['overall_reply_ratio']
+prior = overall_reply_ratio.find_one({'key': 'overall'})['reply_ratio']
 
 app = Flask(__name__)
 
 # Controller: Fetch an email and display it
 @app.route("/will_reply/")
 def will_reply():
+  
+  # Get the message_id, from, first to, and message body
+  message_id = request.args.get('mesage_id')
   froms = request.args.get('from')
   to = request.args.get('to')
   body = request.args.get('body')
   
-  hour = request.args.get('hour') or datetime.time(datetime.now()).hour
-  int_hour = int(hour)
-  if int_hour < 10:
-    hour = "0" + str(int_hour)
+  # For each token in the body, if there's a match in MongoDB, 
+  # append it and average all of them at the end
+  word_probs = []
+  if(body):
+    for token in word_tokenize(body):
+      search = token_reply_rates.find_one({'token': token})
+      if search:
+        word_probs.append(search['reply_rate'])
+    len_probs = float(len(word_probs))
+    if(len_probs > 0):
+      token_rate = sum(word_probs) / len_probs
+    else:
+      token_rate = prior
   else:
-    hour = str(int_hour)
-  print "HOUR: |" + str(int_hour) + "|" + hour + "|"
+    token_rate = prior
   
-  p_from_to_reply = from_to_reply_ratios.find_one({'from': froms, 'to': to})['ratio']
-  p_from_hour = hourly_from_reply_probs.find_one({'address': froms})['sent_distribution'][int_hour]['p_reply']
-
-  result = p_from_hour * .5 + p_from_to_reply * .5
-  
-  print result
+  # Use from/to probabilities when available
+  ftrr = from_to_reply_ratios.find_one({'from': froms, 'to': to})
+  if ftrr:
+    print ftrr
+    p_from_to_reply = ftrr['ratio']
+  else:
+    p_from_to_reply = prior
+  result = (token_rate * .5) + (p_from_to_reply * (1 - .5))
   return str(result)
 
 if __name__ == "__main__":
